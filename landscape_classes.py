a095e207a6962c7806f05f47ca3c36167dc25926
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-03 19:03:54 +0100

    Add file for landscapes. Add draft for grassing for Herbivores in class Low_land

---
+++ landscape_classes.py
@@ -0,0 +1,28 @@
+
+class Low_land():
+    """
+    Doc-strings
+    """
+    def __init__(self, f_max=800.0):
+        self.f_max = f_max ###
+        self.fodder = f_max
+
+    def grassing(self, some_list_of_Herbivores_in_area, F):
+        """
+        Function handling the animals eating in correct order
+
+        Procedure-draft:
+        for Herbivore in sorted list of Herbivores (after fitness-level):
+            if self.fodder >= F:
+                Herbivore.eat_fodder(F) (!F for Herbivores is 10.0) (gains weight at the same time)
+                self.fodder -= F #Adjust available fodder
+            elif:
+                self.fodder > 0 and self.fodder < F:
+                    Herbivore.eat_fodder(self.fodder)
+                    self.fodder = 0
+            else:
+                nothing?
+        """
+        pass
+
+    # Need to reset the amount of fodder by the end of the year
