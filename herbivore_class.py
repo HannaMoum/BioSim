4c552769e1086e008695a72758f15839c41a9002
Cassandra Hjortdahl <cassandra.hjortdahl@nmbu.no>
2022-01-03 14:03:44 +0100

    Create draft for herbivore class with weight-methods.

--- herbivore_class.py
+++ herbivore_class.py
@@ -1,3 +1,29 @@
 
 class Herbivore:
-    pass
+    """"
+    Legg inn doc-string
+    """
+
+    def __init__(self, age = 0, weight = None):
+        self.age = age
+        self.weight = weight
+
+    def determine_birthweight(self, w_birth, sigma_birth):
+        """
+        Funksjon som avgjør fødselsvekten basert på mean og standard deviation. Gaussian dist.
+        """
+        pass
+
+    def increase_weight_when_eating(self, F_tilde, beta):
+        """
+        Funksjon som regner ut vektøkningen etter at dyret har spist
+        beta*F_tilde
+        """
+        pass
+
+    def decrease_weight_when_aging(self, eta):
+        """
+        Funksjon som regner ut hvor mye vekten minker per år
+        eta*weight
+        """
+        pass
