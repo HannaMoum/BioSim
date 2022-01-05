34c91c20c24943ad3760006eab87aeba6b4f48ef
Cassandra Hjortdahl <cassandra.hjortdahl@nmbu.no>
2022-01-05 15:35:01 +0100

    Remove comment in params.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -15,7 +15,7 @@ class Herbivore:
         'w_birth': 8,
         'sigma_birth': 1.5,
         'beta': 0.9,
-        'eta': 0.05, # Mindre eller lik 1
+        'eta': 0.05,
         'a_half': 40.0,
         'phi_age': 0.6,
         'w_half': 10.0,
