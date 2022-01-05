c66f00e36688c13a8cec7ec5e0b76ca987b520f9
Cassandra Hjortdahl <cassandra.hjortdahl@nmbu.no>
2022-01-05 15:50:15 +0100

    Add names for required tests for animals.

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -2,7 +2,55 @@ import pytest
 from biosim.animals import Herbivore
 
 @pytest.mark.skip('Not finished')
+def test_set_params():
+    pass
+
+@pytest.mark.skip('Not finished')
+def test_count_new_herbi():
+    pass
+
+@pytest.mark.skip('Not finished')
+def test_num_herbis():
+    pass
+
+@pytest.mark.skip('Not finished')
+def test_init():
+    pass
+
+@pytest.mark.skip('Not finished')
 def test_find_birthweight():
     pass
 
-def
+@pytest.mark.skip('Not finished')
+def test_eat()
+    pass
+
+@pytest.mark.skip('Not finished')
+def test__q():
+    pass
+
+@pytest.mark.skip('Not finished')
+def test_fitness():
+    pass
+
+@pytest.mark.skip('Not finished')
+def test_decrease_weight_when_aging():
+    pass
+
+@pytest.mark.skip('Not finished')
+def test_aging():
+    pass
+
+@pytest.mark.skip('Not finished')
+def test_probability_to_give_birth():
+    pass
+
+@pytest.mark.skip('Not finished')
+def test_giving_birth():
+    pass
+
+@pytest.mark.skip('Not finished')
+def test_death():
+    pass
+
+
