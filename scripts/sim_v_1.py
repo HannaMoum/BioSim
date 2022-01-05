7139179172a81a638b9f6d948fcc7c25a278a7ac
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-05 16:28:38 +0100

    First draft for herbivore-lowland simulation

--- scripts/sim_v_1.py
+++ scripts/sim_v_1.py
@@ -1,2 +1,18 @@
 """ Simulating only herbivores and lowland
 """
+from biosim.animals import Herbivore
+from biosim.landscapes import Lowland
+
+sim = Lowland(5)
+
+def cycle(sim):
+    sim.grassing()
+    sim.give_birth()
+    sim.aging()
+    sim.death()
+    sim.regrowth()
+
+
+for year in range(100):
+    cycle(sim)
+    print(len(sim.herb_pop))
