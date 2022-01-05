b10eada280aa93cc45b1a18054224b7e3e66630e
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-05 15:04:10 +0100

    Change parameters for Lowland, remove comments.

--- src/biosim/landscapes.py
+++ src/biosim/landscapes.py
@@ -8,7 +8,6 @@ class Lowland:
 
     params = {
         'f_max': 800.0,
-        'F': 10.0
     }
 
     def __init__(self, num_herb):
@@ -21,7 +20,6 @@ class Lowland:
         """
         Function handling the animals eating in correct order
         """
-        #F_satisfied = self.params['F']
         # Sort list, highest fitness first:
         for herbivore in sorted(self.herb_pop, key=lambda x: x.fitness, reverse=True):
 
@@ -31,18 +29,6 @@ class Lowland:
             if self.fodder == 0:
                 break
 
-            # if self.fodder >= F_satisfied:
-            #     herbivore.eat(F_satisfied)  # Gains weight
-            #     self.fodder -= F_satisfied  # Adjust available fodder
-            #
-            # elif self.fodder > 0:  # More than zero, but less than F_satisfied
-            #     herbivore.eat(self.fodder)
-            #     self.fodder = 0
-            #
-            # else:
-            #     break  # No reason to continue looping if there are no more fodder available.
-
-    # vars(self.f_max) (https://www.programiz.com/python-programming/methods/built-in/vars)
 
     def give_birth(self):
         """
@@ -65,7 +51,7 @@ class Lowland:
         """
         Method to reset the amount of fodder by the end of the year
         """
-        self.fodder = self.f_max # Check if this is a pointer or a copy
+        self.fodder = self.f_max  # Check if this is a pointer or a copy
 
     def death(self):
         def survivors(pop):
