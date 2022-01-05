8cfef2917faa85a7770da56e8984ba6513a0c799
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-05 14:28:20 +0100

    Adapt grassing method to changes made in animals

--- src/biosim/landscapes.py
+++ src/biosim/landscapes.py
@@ -21,20 +21,28 @@ class Lowland:
         """
         Function handling the animals eating in correct order
         """
-        F_satisfied = self.params['F']
+        #F_satisfied = self.params['F']
         # Sort list, highest fitness first:
         for herbivore in sorted(self.herb_pop, key=lambda x: x.fitness, reverse=True):
 
-            if self.fodder >= F_satisfied:
-                herbivore.eat(F_satisfied)  # Gains weight
-                self.fodder -= F_satisfied  # Adjust available fodder
+            herbivore.eat(self.fodder)
 
-            elif self.fodder > 0:  # More than zero, but less than F_satisfied
-                herbivore.eat(self.fodder)
-                self.fodder = 0
+            eaten = herbivore.eat(self.fodder)
+            self.fodder -= eaten
 
-            else:
-                break  # No reason to continue looping if there are no more fodder available.
+            if self.fodder == 0:
+                break
+
+            # if self.fodder >= F_satisfied:
+            #     herbivore.eat(F_satisfied)  # Gains weight
+            #     self.fodder -= F_satisfied  # Adjust available fodder
+            #
+            # elif self.fodder > 0:  # More than zero, but less than F_satisfied
+            #     herbivore.eat(self.fodder)
+            #     self.fodder = 0
+            #
+            # else:
+            #     break  # No reason to continue looping if there are no more fodder available.
 
     # vars(self.f_max) (https://www.programiz.com/python-programming/methods/built-in/vars)
 
