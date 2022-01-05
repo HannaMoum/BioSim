7c30b002c576500692cd401a325573c45e03cb7b
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-05 16:47:12 +0100

    Adding comments

--- scripts/sim_v_1.py
+++ scripts/sim_v_1.py
@@ -3,7 +3,10 @@
 from biosim.animals import Herbivore
 from biosim.landscapes import Lowland
 
-sim = Lowland(5)
+sim = Lowland(20)
+#new_params = {'omega': 0.2} Does not work because this parameter is for Herbivore and not Lowland
+#sim.set_params(new_params)
+#print(sim.params)
 
 def cycle(sim):
     sim.grassing()
@@ -12,7 +15,6 @@ def cycle(sim):
     sim.death()
     sim.regrowth()
 
-
-for year in range(100):
+for year in range(10):
     cycle(sim)
     print(len(sim.herb_pop))
