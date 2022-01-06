1a329eda8d6f7078ed73c64933ad6ad4a7f204bf
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-06 20:05:19 +0100

    Add drafts for init and grassing testing.

--- tests/test_landscape.py
+++ tests/test_landscape.py
@@ -5,7 +5,9 @@ from biosim.landscapes import Lowland
 SEED = 12345678  # random seed for tests
 
 def test_set_params():
-    """ Testing change of parameter default value (f_max)"""
+    """ Testing change of parameter default value (f_max)
+        Testing raise of error if wrongful params have been given"""
+    #Split in severeal? Keep as one?...
     wanted_param = {'f_max': 600}
     wrongful_param = {'f_min': 600}
     wrongful_value = {'f_max': -600}
@@ -19,3 +21,19 @@ def test_set_params():
 
     with pytest.raises(ValueError):
         Lowland.set_params(wrongful_value)
+
+def test_init_fodder():
+    pass
+
+def test_init_herb_pop():
+    pass
+
+def test_grassing():
+    """
+    Test they are eating in correct order (test sorting-funtion)
+    Test for weightgain after eating?
+    Test for the correctly updated value of fodder
+    Test that a loop break happens under correct circumstances
+    """
+    #herbivores = [Herb]
+    grass = Lowland()
