8fdcf36275dd0b49ab466660d610aefd57f0ae9c
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-06 16:22:44 +0100

    Getting simulation file to run

--- scripts/sim_v_1.py
+++ scripts/sim_v_1.py
@@ -9,18 +9,20 @@ ini_herb_pop = [{'Species': 'Herbivore', 'age': 10, 'weight': 12.5},
                 {'Species': 'Herbivore', 'age': 10, 'weight': 12.5},
                 {'Species': 'Herbivore', 'age': 9, 'weight': 10.3}]
 
+# Adjusting parameters
+#Herbivore.set_params(new_params)
+#Herbivore.set_params({'omega': 0.2, 'gamma': 0.7})
+
 # Turning initial list of information into list of Herbivores
 initial = []
 for animal in ini_herb_pop:
     if animal['Species'] == 'Herbivore':
         initial.append(Herbivore(animal['age'], animal['weight']))
-print(initial)
 
-#Herbivore.set_params({'omega': 0.2, 'gamma': 0.7})
 sim = Lowland(initial)
 
-new_params = {'f_max': 800}
-sim.set_params(new_params)
+#new_params = {'f_max': 800}
+#sim.set_params(new_params)
 
 def cycle(sim):
     sim.grassing()
@@ -29,6 +31,6 @@ def cycle(sim):
     sim.death()
     sim.regrowth()
 
-for year in range(10):
+for year in range(100):
     cycle(sim)
     print(len(sim.herb_pop))
