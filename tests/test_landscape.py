4c9cdb40a0054e018e62955e9f52474c4b7fda01
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-06 19:46:48 +0100

    Add test for set_params

--- tests/test_landscape.py
+++ tests/test_landscape.py
@@ -3,3 +3,10 @@ from biosim.landscapes import Lowland
 
 #  Overall parameters for probabilistic tests
 SEED = 12345678  # random seed for tests
+
+def test_set_params():
+    """ Testing change of parameter default value (f_max)"""
+    wanted_param = {'f_max': 600}
+    Lowland.set_params(wanted_param)
+    new_param = Lowland.params['f_max']
+    assert wanted_param['f_max'] == new_param
