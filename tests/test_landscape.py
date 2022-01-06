e19e4711aa59ec3d2b9eacf42b3fea7f00aeee0e
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-06 19:56:19 +0100

    Extending set_params test

--- tests/test_landscape.py
+++ tests/test_landscape.py
@@ -7,6 +7,15 @@ SEED = 12345678  # random seed for tests
 def test_set_params():
     """ Testing change of parameter default value (f_max)"""
     wanted_param = {'f_max': 600}
+    wrongful_param = {'f_min': 600}
+    wrongful_value = {'f_max': -600}
+
     Lowland.set_params(wanted_param)
     new_param = Lowland.params['f_max']
     assert wanted_param['f_max'] == new_param
+
+    with pytest.raises(KeyError):
+        Lowland.set_params(wrongful_param)
+
+    with pytest.raises(ValueError):
+        Lowland.set_params(wrongful_value)
