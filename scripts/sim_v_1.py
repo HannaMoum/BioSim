5cd3be4ad4addc86a4ee79e30588fbfed8498888
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-06 16:31:44 +0100

    Tidying

--- scripts/sim_v_1.py
+++ scripts/sim_v_1.py
@@ -10,26 +10,30 @@ ini_herb_pop = [{'Species': 'Herbivore', 'age': 10, 'weight': 12.5},
                 {'Species': 'Herbivore', 'age': 9, 'weight': 10.3}]
 
 # Adjusting parameters
-#Herbivore.set_params(new_params)
-#Herbivore.set_params({'omega': 0.2, 'gamma': 0.7})
+# new_params_herbivore = {'omega': 0.7, 'gamma': 0.3}
+# Herbivore.set_params(new_params_herbivore)
+# new_params_landscape = {'f_max': 600}
+# Lowland.set_params(new_params_landscape)
 
 # Turning initial list of information into list of Herbivores
-initial = []
+initial_pop = []
 for animal in ini_herb_pop:
     if animal['Species'] == 'Herbivore':
-        initial.append(Herbivore(animal['age'], animal['weight']))
+        initial_pop.append(Herbivore(animal['age'], animal['weight']))
+for an in initial_pop:
+    print(an.params)
 
-sim = Lowland(initial)
 
-#new_params = {'f_max': 800}
-#sim.set_params(new_params)
+sim = Lowland(initial_pop)
 
-def cycle(sim):
-    sim.grassing()
-    sim.give_birth()
-    sim.aging()
-    sim.death()
-    sim.regrowth()
+
+def cycle(simulation):
+    simulation.grassing()
+    simulation.give_birth()
+    simulation.aging()
+    simulation.death()
+    simulation.regrowth()
+
 
 for year in range(100):
     cycle(sim)
