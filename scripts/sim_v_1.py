0e896984ba13d005a662b93bc605f309d01209d4
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-06 13:56:39 +0100

    Creating list of herbivores from list of initial_population

--- scripts/sim_v_1.py
+++ scripts/sim_v_1.py
@@ -3,16 +3,24 @@
 from biosim.animals import Herbivore
 from biosim.landscapes import Lowland
 
+# Creating initial population as list/dicts
+ini_herb_pop = [{'Species': 'Herbivore', 'age': 10, 'weight': 12.5},
+                {'Species': 'Herbivore', 'age': 9, 'weight': 10.3},
+                {'Species': 'Herbivore', 'age': 10, 'weight': 12.5},
+                {'Species': 'Herbivore', 'age': 9, 'weight': 10.3}]
+
+# Turning initial list of information into list of Herbivores
+initial = []
+for animal in ini_herb_pop:
+    if animal['Species'] == 'Herbivore':
+        initial.append(Herbivore(animal['age'], animal['weight']))
+print(initial)
+
 #Herbivore.set_params({'omega': 0.2, 'gamma': 0.7})
-sim = Lowland(20)
+sim = Lowland(initial)
 
 new_params = {'f_max': 800}
 sim.set_params(new_params)
-
-ini_herb_pop = [{'Species': 'Herbivore', 'age': 10, 'weight': 12.5},
-                {'species': 'Herbivore', 'age': 9, 'weight': 10.3},
-                {'Species': 'Herbivore', 'age': 10, 'weight': 12.5},
-                {'species': 'Herbivore', 'age': 9, 'weight': 10.3}]
 
 def cycle(sim):
     sim.grassing()
