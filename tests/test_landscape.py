709b137ff3e721bd5e2b45f36e1f1fc0f6901441
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-13 22:11:11 +0100

    Add test for hunting

--- tests/test_landscape.py
+++ tests/test_landscape.py
@@ -1,5 +1,5 @@
 import pytest
-from biosim.animals import Herbivore
+from biosim.animals import Herbivore, Carnivore
 from biosim.lowland import Landscape
 
 #  Overall parameters for probabilistic tests
@@ -55,26 +55,26 @@ def test_init_f_max_zero(terrain):
     assert Landscape(terrain).f_max == 0
 
 
-@pytest.mark.parametrize('terrain', ['L', 'W', 'D', 'W'])
+@pytest.mark.parametrize('terrain', ['L', 'H', 'D', 'W'])
 def test_initial_fodder(terrain):
     """Test that initial amount of fodder is equal to f_max."""
     location_cell = Landscape(terrain)
     assert location_cell.fodder == location_cell.f_max
 
 
-@pytest.mark.parametrize('terrain', ['L', 'W', 'D', 'W'])
+@pytest.mark.parametrize('terrain', ['L', 'H', 'D', 'W'])
 def test_initial_population_type(terrain):
     """Test that initial population type is a list."""
     assert all([type(Landscape(terrain).herb_pop) == list, type(Landscape(terrain).carn_pop) == list])
 
 
-@pytest.mark.parametrize('terrain', ['L', 'W', 'D', 'W'])
+@pytest.mark.parametrize('terrain', ['L', 'H', 'D', 'W'])
 def test_inital_population(terrain):
     """Test for no initial population. An emtpy list returns False"""
     assert not all([Landscape(terrain).herb_pop, Landscape(terrain).carn_pop])
 
 
-### TODO: Is any of this valuable?
+### TODO: Is any of this valuable? HOW TO AVOID ADDING ANIMALS TO WATER
 @pytest.mark.parametrize('terrain_letter, terrain', [('L', 'Lowland'), ('H', 'Highland')]) #TODO: Not water here!
 def test_grassing_fodder_adjustment(terrain_letter, terrain):
     """Test fodder adjustment when grassing in Lowland and Highland while there are still fodder available.
@@ -105,40 +105,37 @@ def test_correct_eating_order():
 
     assert sorted_herbivores == [first_eater, second_eater, third_eater]
 
+@pytest.mark.parametrize('terrain', ['L', 'H', 'D']) #! No water
+def test_hunting_no_carnivores(terrain):
+    """Test for sorted updated herbivore population with the same amount of herbivores,
+    when no carnivores are present."""
+    first_prey = Herbivore(0, 3)
+    second_prey = Herbivore(Herbivore.params['w_half'], Herbivore.params['a_half'])
+    third_prey = Herbivore(12.5, 10)
+    preys = [second_prey, third_prey, first_prey]
 
-@pytest.mark.skip
-def test_grassing_no():
-    """
-    Test they are eating in correct order (test sorting-funtion) #How?...
-    Test for weightgain after eating? #SHOULD BE TESTED IN test_eat, transferred
-    Test for the correctly updated value of fodder #test here
-    Test that a loop break happens under correct circumstances #test here
-    """
+    location_cell = Landscape(terrain)
+    location_cell.herb_pop += preys
+    location_cell.hunting()
 
-    # Test fodder reduction
-    herbivores = [Herbivore(10, 12.5), Herbivore(9, 10.3), Herbivore(3, 6.8)]
-    loc = Lowland(herbivores)
-    loc.grassing()
-    # Assumption: Enough food available. Need some adjustments
-    assert Lowland.params['f_max'] - len(herbivores)*Herbivore.params['F'] == loc.fodder
+    assert location_cell.herb_pop == [first_prey, second_prey, third_prey]
 
-    # Transferred this code to test_eat in test_animals instead.
-    # Delete everything, should it be tested here...?
 
-    # herbivores_i = [Herbivore(10, 12.5), Herbivore(9, 10.3), Herbivore(3, 6.8)]  # before eating
-    # herbivores_f = [Herbivore(10, 12.5), Herbivore(9, 10.3), Herbivore(3, 6.8)]  # after eating
-    #
-    # #Weightgain
-    # gain = Herbivore.params['F'] * Herbivore.params['beta']
-    #
-    # initial = Lowland(herbivores_i).herb_pop
-    #
-    # final_loc = Lowland(herbivores_f)
-    # final_loc.grassing()
-    # final = final_loc.herb_pop
-    #
-    # for herbivore_i, herbivore_f in zip(initial, final):
-    #
-    #     assert herbivore_i._weight + gain == herbivore_f._weight
-    pass
+@pytest.mark.parametrize('terrain', ['L', 'H', 'D']) #! No water
+def test_hunting_no_killing(terrain):
+    """Test for steady herbivore population, if no killing takes place.
+    Parameter adjustments assures no killing (no hungry carnivores)."""
+    Carnivore.set_params({'F': 0})
+
+    preys = [Herbivore(Herbivore.params['w_half'], Herbivore.params['a_half']),
+             Herbivore(12.5, 10), Herbivore(0, 3)]
+    hunters = [Carnivore(Carnivore.params['w_half'], Carnivore.params['a_half']),
+             Carnivore(12.5, 10), Carnivore(0, 3)]
+
+    location_cell = Landscape(terrain)
+    location_cell.herb_pop += preys
+    location_cell.carn_pop += hunters
+    location_cell.hunting()
+
+    assert len(preys) == len(location_cell.herb_pop)
 
