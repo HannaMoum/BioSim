2f85cb89ac8557abb1b102e619e6527472e2d4c9
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-15 14:42:52 +0100

    Adjust test for adding animals successfully

--- tests/test_island.py
+++ tests/test_island.py
@@ -2,6 +2,7 @@ import textwrap
 import pytest
 from biosim.world import World
 import numpy as np
+from biosim.landscape import Landscape
 
 SEED = 12345678  # random seed for tests
 
@@ -94,23 +95,20 @@ def test_object_map_save(geogr_str):
     assert object_map_creation == object_map_attribute  # Not sure this will work...
 
 
-@pytest.mark.skip
 def test_object_map_shape(geogr_str):
     """Test creation of correct size for object_map."""
     island = World(geogr_str)
     assert island.object_map.shape == (3, 4)
 
 
-@pytest.mark.skip
 def test_object_map_type(geogr_str):
     """Test correct creation of object references by checking their type."""
     island = World(geogr_str)
     for reference_row in island.object_map:
         for reference in reference_row:
-            assert type(reference) == object  # Or something like this...
+            assert type(reference) == Landscape
 
 
-@pytest.mark.skip
 def test_object_map_reference(geogr_str):
     """Test correct creation of object references by checking their attribute landscape_type."""
     island = World(geogr_str)
@@ -120,7 +118,6 @@ def test_object_map_reference(geogr_str):
             assert reference.landscape_type == letter
 
 
-@pytest.mark.skip
 def test_object_map_coordinate(geogr_str):
     """Test that given coordinates picks out the correct object."""
     island = World(geogr_str)
@@ -128,7 +125,6 @@ def test_object_map_coordinate(geogr_str):
                 island.object_map[1, 2].landscape_type == 'H'])
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize('location', [(1, -1), (-1, 1), (0, 1), (9, 2), (2, 9)])
 def test_add_animals_IndexError(geogr_str, location):
     """Test that IndexError arise if negative coordinates or non-existent coordinates are provided."""
@@ -139,13 +135,15 @@ def test_add_animals_IndexError(geogr_str, location):
 
 
 @pytest.mark.parametrize('location', [(2, 2), (2, 3)])
-@pytest.mark.skip
 def test_add_animals_success(geogr_str, location):
     """Test that animals are added in the correct locations."""
     island = World(geogr_str)
     add_pop = [{'loc': location, 'pop': [{'species': 'Herbivore', 'age': 5, 'weight': 5},
                                          {'species': 'Carnivore', 'age': 6, 'weight': 6.5}]}]
     island.add_population(add_pop)
-    assert all([len(island.object_map[location].population) == 2,
-                len(island.object_map[location].herbivores) == 1,
-                len(island.object_map[location].carnivores) == 1])
+    row, col = location
+    adjust_row = row - 1
+    adjust_col = col - 1
+    assert all([len(island.object_map[adjust_row, adjust_col].population) == 2,
+                len(island.object_map[adjust_row, adjust_col].herbivores) == 1,
+                len(island.object_map[adjust_row, adjust_col].carnivores) == 1])
