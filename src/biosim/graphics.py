642d3d4cf73856c481e248f3e0d85aac57c7b9ee
Cassandra Hjortdahl <cassandra.hjortdahl@nmbu.no>
2022-01-15 23:50:55 +0100

    Tidy Graphics.

--- src/biosim/graphics.py
+++ src/biosim/graphics.py
@@ -6,15 +6,13 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 import seaborn as sns
 
-#pip install moviepy. Må pip installeres i environmentet du jobber i
-from moviepy.editor import VideoClip
+# pip install moviepy. Må pip installeres i environmentet du jobber i
 from moviepy.video.io.bindings import mplfig_to_npimage
 import moviepy.video.io.ImageSequenceClip
 
-from datetime import datetime
 
 @dataclass
-class Graphics_param:
+class GraphicsParams:
     ymax_animals: int
     cmax_animals_herbivore: int = 50
     cmax_animals_carnivore: int = 50
@@ -32,30 +30,27 @@ class Graphics_param:
 
     codes_for_landscape_types: str = 'WLHD'
 
-
-    def code_landscape(self, value): # Finn mer beskrivende funksjonsnavn
+    def code_landscape(self, value):  # Finn mer beskrivende funksjonsnavn
         # TODO: Funksjonen må oppdateres med å sjekke at input value er lovlig.
         plot_values_for_landscape_types = '0123'
         # Vurder å ha de som parametere, sånn at de kan brukes globalt
         if value in self.codes_for_landscape_types:
             replacement_values = list(zip(self.codes_for_landscape_types, plot_values_for_landscape_types))
-            for letter,number in replacement_values:
+            for letter, number in replacement_values:
                 if value == letter:
                     return int(number)
 
-    # def update_params(self, new_dict):
-    #     for key, value in new_dict.items():
-    #         if hasattr(self, key):
-    #             setattr(self, key, value)
 
-class Graphics(Graphics_param):
+class Graphics(GraphicsParams):
 
-    def __init__(self, numpy_island_map, hist_specs:dict, ymax_animals:int, cmax_animals:dict, vis_years:int,
-                 img_dir, img_base, img_fmt, img_years):
+    def __init__(self, numpy_island_map, hist_specs: dict,
+                 ymax_animals: int, cmax_animals: dict,
+                 vis_years: int, img_dir, img_base,
+                 img_fmt, img_years):
         """
         numpy_island_map er base_map fra World.
         """
-        self._island_plot = self.make_plot_map(numpy_island_map)
+        self._island_plot = self._make_plot_map(numpy_island_map)
         self._set_hist_specs(hist_specs)
         self.ymax_animals = ymax_animals
         self._set_cmax_animals(cmax_animals)
@@ -87,27 +82,30 @@ class Graphics(Graphics_param):
         if cmax_animals is None:
             return True
 
-
         for key, value in cmax_animals.items():
             if key == 'Herbivore':
                 self.cmax_animals_herbivore = value
             if key == 'Carnivore':
                 self.cmax_animals_carnivore = value
 
-    def make_plot_map(self, numpy_island_map):
-        """Lager numpy array (kartet) som brukes for å plotte verdenskartet."""
-        island_map_plot = np.copy(
-            numpy_island_map)  # Lager intern kopi, for å unngå å ødelegge opprinnelig array. Hadde bare blitt et view om man ikke hadde gjort det.
-        vcode_landscape = np.vectorize(
-            self.code_landscape)  # Vektoriserer funksjonen. Funksjonen kan da benyttes celle for celle i arrayen (eller et utvalg av arrayen).
-        island_map_plot[:, :] = vcode_landscape(
-            island_map_plot)  # Bruker den vektoriserte funksjonen element for element i slicen som er laget (her alle celler).
-        island_map_plot = np.array(island_map_plot, dtype=int)  # Konverterer fra siffer (tall som str) til tall (int).
+    def _make_plot_map(self, numpy_island_map):
+        """
+        Lager numpy array (kartet) som brukes for å plotte verdenskartet.
+        """
+        # Lager intern kopi, for å unngå å ødelegge opprinnelig array.
+        # Hadde bare blitt et view om man ikke hadde gjort det.
+        island_map_plot = np.copy(numpy_island_map)
+        # Vektoriserer funksjonen. Funksjonen kan da benyttes celle for celle i arrayen (eller et utvalg av arrayen).
+        vcode_landscape = np.vectorize(self.code_landscape)
+        # Bruker den vektoriserte funksjonen element for element i slicen som er laget (her alle celler).
+        island_map_plot[:, :] = vcode_landscape(island_map_plot)
+        # Konverterer fra siffer (tall som str) til tall (int).
+        island_map_plot = np.array(island_map_plot, dtype=int)
 
         return island_map_plot
         # Returnerer np array med verdiene 0 til 3, som kan brukes til plotting.
 
-    def plot_island_map(self, ax):
+    def _plot_island_map(self, ax):
         """Plotter verdenskartet"""
         plot_map = self._island_plot
         row, col = plot_map.shape
@@ -116,13 +114,13 @@ class Graphics(Graphics_param):
         ax.imshow(plot_map, cmap=colormap, extent=[1, col + 1, row + 1, 1])
         ax.set_xticks(range(1, col + 1))
         ax.set_yticks(range(1, row + 1))
-        ax.set_xticklabels(range(1, col + 1), rotation = 90)
+        ax.set_xticklabels(range(1, col + 1), rotation=90)
         ax.set_yticklabels(range(1, row + 1))
-        ax.set(title = 'Island map')
+        ax.set(title='Island map')
 
         return ax
 
-    def plot_heatmap(self, data: object, species: str, ax, year: int = -1):
+    def _plot_heatmap(self, data: object, species: str, ax, year: int = -1):
         """Plotter heatmap"""
         if species == 'Herbivore':
             title = 'Herbivore distribution'
@@ -135,14 +133,14 @@ class Graphics(Graphics_param):
         else:
             raise ValueError('Feil')
         # TODO: Fjerne annot som argument hvis vi bestemmer oss for å ikke ha tall i heatmapet.
-        ax = sns.heatmap(data[year, :, :], annot=False, cmap=cmap, ax = ax,
-                         center = center, xticklabels = [x for x in range(1, data.shape[2] + 1)],
-                         yticklabels = [x for x in range(1, data.shape[1] + 1)])
+        ax = sns.heatmap(data[year, :, :], annot=False, cmap=cmap, ax=ax,
+                         center=center, xticklabels=[x for x in range(1, data.shape[2] + 1)],
+                         yticklabels=[x for x in range(1, data.shape[1] + 1)])
         ax.set_title(title)
 
         return ax
 
-    def plotting_population_count(self, herb_data: object, carn_data: object, ax: object, year):
+    def _plotting_population_count(self, herb_data: object, carn_data: object, ax: object, year):
         """
         Brukes til å plotte population size over tid
         Data er np array, med en sum per år i simuleringen
@@ -159,8 +157,10 @@ class Graphics(Graphics_param):
 
         return ax
 
-    def plot_histogram(self, hist_herb_data:list, hist_carn_data:list, ax_age, ax_weight, ax_fitness, year: int = -1)-> object:
-        colors = ['green', 'red']
+    def _plot_histogram(self, hist_herb_data: list, hist_carn_data: list,
+                        ax_age, ax_weight, ax_fitness,
+                        year: int = -1) -> object:
+        hist_colors = ['green', 'red']
 
         yearly_herb_data = hist_herb_data[year]
         yearly_carn_data = hist_carn_data[year]
@@ -168,19 +168,19 @@ class Graphics(Graphics_param):
         if yearly_carn_data.shape == (0,):
             yearly_carn_data = np.zeros(shape=(1, 3))
         if yearly_herb_data.shape == (0,):
-            yearly_herb_data = np.zeros(shape=(1,3))
+            yearly_herb_data = np.zeros(shape=(1, 3))
 
         # Age
-        ax_age.hist([yearly_herb_data[:, 0],yearly_carn_data[:, 0]],
-                    bins=int(self.age_max/self.age_delta),
-                    range=(0,self.age_max),
-                    histtype= 'step',
+        ax_age.hist([yearly_herb_data[:, 0], yearly_carn_data[:, 0]],
+                    bins=int(self.age_max / self.age_delta),
+                    range=(0, self.age_max),
+                    histtype='step',
                     stacked=False,
                     fill=False,
-                    color=colors,
+                    color=hist_colors,
                     label=['Herbivore', 'Carnivore'])
         ax_age.set(xlim=(0, self.age_max),
-                   title = 'Age')
+                   title='Age')
 
         # Weight
         ax_weight.hist([yearly_herb_data[:, 1], yearly_carn_data[:, 1]],
@@ -189,10 +189,10 @@ class Graphics(Graphics_param):
                        histtype='step',
                        stacked=False,
                        fill=False,
-                       color=colors,
+                       color=hist_colors,
                        label=['Herbivore', 'Carnivore'])
         ax_weight.set(xlim=(0, self.weight_max),
-                      title = 'Weight')
+                      title='Weight')
 
         # Fitness
         ax_fitness.hist([yearly_herb_data[:, 2], yearly_carn_data[:, 2]],
@@ -201,63 +201,69 @@ class Graphics(Graphics_param):
                         histtype='step',
                         stacked=False,
                         fill=False,
-                        color=colors,
-                        label = ['Herbivore', 'Carnivore'])
-        ax_fitness.legend(bbox_to_anchor = (1.01, 1))
+                        color=hist_colors,
+                        label=['Herbivore', 'Carnivore'])
+        ax_fitness.legend(bbox_to_anchor=(1.01, 1))
         ax_fitness.set(xlim=(0, self.fitness_max),
-                      title='Fitness')
+                       title='Fitness')
 
         return ax_age, ax_weight, ax_fitness
 
-    def show_grid(self, data_heat_herb, data_heat_carn, herb_data, carn_data, hist_herb_data, hist_carn_data, pause, year, show:bool, save: bool):
-        fig = self._make_grid(data_heat_herb, data_heat_carn,
-                              herb_data, carn_data,
-                              hist_herb_data,
-                              hist_carn_data, pause, year)
-        if show:
-            plt.pause(pause)
-
-        if save:
-            self._save_grid(fig, year)
-
-    def _make_grid(self, data_heat_herb, data_heat_carn, herb_data, carn_data, hist_herb_data, hist_carn_data, pause, year):
+    def _make_grid(self, data_heat_herb, data_heat_carn,
+                   herb_data, carn_data,
+                   hist_herb_data, hist_carn_data, year):
         plot_year = year
-        year -= 1 # Må ta -1 fordi dataene er null-basert (første index er 0)
+        year -= 1  # Må ta -1 fordi dataene er null-basert (første index er 0)
 
         fig = plt.figure(figsize=(14, 10))
-        fig.suptitle(str(f'Year: {(plot_year):.0f}'), fontsize=36, x=0.15, y=0.95)
+        fig.suptitle(str(f'Year: {plot_year:.0f}'), fontsize=36, x=0.15, y=0.95)
 
         grid = plt.GridSpec(10, 14, wspace=0.75, hspace=1.5)
 
         map_ax = plt.subplot(grid[0:3, 0:4])
-        self.plot_island_map(map_ax)
+        self._plot_island_map(map_ax)
 
         herb_heatax = plt.subplot(grid[0:3, 4:9])
-        self.plot_heatmap(data_heat_herb, 'Herbivore', herb_heatax, year = year)
+        self._plot_heatmap(data_heat_herb, 'Herbivore', herb_heatax, year=year)
 
         carn_heatax = plt.subplot(grid[0:3, 9:14])
-        self.plot_heatmap(data_heat_carn, 'Carnivore', carn_heatax, year = year)
+        self._plot_heatmap(data_heat_carn, 'Carnivore', carn_heatax, year=year)
 
         pop_ax = plt.subplot(grid[4:10, 0:5])
-        self.plotting_population_count(herb_data, carn_data, pop_ax, year = year)
+        self._plotting_population_count(herb_data, carn_data, pop_ax, year=year)
 
         age_ax = plt.subplot(grid[6:8, 6:13])
         weight_ax = plt.subplot(grid[8:10, 6:13])
         fitness_ax = plt.subplot(grid[4:6, 6:13])
-        self.plot_histogram(hist_herb_data, hist_carn_data, age_ax, weight_ax, fitness_ax, year)
+        self._plot_histogram(hist_herb_data, hist_carn_data, age_ax, weight_ax, fitness_ax, year)
 
         return fig
 
     def _save_grid(self, fig: object, year: int):
         fig.savefig(f'{self.img_dir}/{self.img_base}_{year:05d}.{self.img_fmt}', format=self.img_fmt)
 
+    def show_grid(self, data_heat_herb, data_heat_carn,
+                  herb_data, carn_data,
+                  hist_herb_data, hist_carn_data,
+                  pause, year, show: bool, save: bool):
+        fig = self._make_grid(data_heat_herb, data_heat_carn,
+                              herb_data, carn_data,
+                              hist_herb_data,
+                              hist_carn_data, year)
+        if show:
+            plt.pause(pause)
+
+        if save:
+            self._save_grid(fig, year)
+
     def make_movie_from_files(self):
         fps = 1
         image_files = [os.path.join(self.img_dir, img)
                        for img in os.listdir(self.img_dir)
-                       if img.endswith("."+self.img_fmt)]# Lager en liste over alle filene
+                       if img.endswith("." + self.img_fmt)]  # Lager en liste over alle filene
         image_files.sort()
 
         filename = f'{self.img_dir}/{self.img_base}_video.mp4'
-        clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps) # Går gjennom et og et bilde og bygger opp video-kuben.
-        clip.write_videofile(filename) # Lager det om til en videofil.
+        # Går gjennom et og et bilde og bygger opp video-kuben.
+        clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+        clip.write_videofile(filename)  # Lager det om til en videofil.
