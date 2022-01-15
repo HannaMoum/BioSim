1c88d96c4d8869d6fd591a3be8972074260f1eec
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-15 16:08:01 +0100

    Add test for get_property_map

--- tests/test_island.py
+++ tests/test_island.py
@@ -147,3 +147,24 @@ def test_add_animals_success(geogr_str, location):
     assert all([len(island.object_map[adjust_row, adjust_col].population) == 2,
                 len(island.object_map[adjust_row, adjust_col].herbivores) == 1,
                 len(island.object_map[adjust_row, adjust_col].carnivores) == 1])
+
+
+def test_get_property_map_herb_count(geogr_str):
+    """Test get_ and make_property_map for herbivores by checking expected population."""
+    island = World(geogr_str)
+    add_pop = [{'loc': (2, 2), 'pop': [{'species': 'Herbivore', 'age': 5, 'weight': 5},
+                                       {'species': 'Carnivore', 'age': 6, 'weight': 6.5}]}]
+    island.add_population(add_pop)
+    herb_map = island.get_property_map('v_size_herb_pop')
+    zeros = sum(True for sub_array in herb_map for number in sub_array if not number)
+    row, col = herb_map.shape
+    expected_zeros = (row * col) - 1
+
+    assert all([herb_map[1, 1] == 1, zeros == expected_zeros])
+
+
+
+
+
+
+
