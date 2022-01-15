50be55b0ee7a91489be436308ff90fdc95c1e955
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-15 19:30:26 +0100

    Add tests to assure correct migration direction, and no migration to water.

--- tests/test_island.py
+++ tests/test_island.py
@@ -177,6 +177,36 @@ def test_get_property_map_objects(geogr_str, function_call, species):
     assert all([attributes[1, 1] == [(6, 6.5, species(6.5, 6).fitness)], nones == expected_nones])
 
 
+def test_migration(mocker, geogr_str):
+    """Test correct migration of animal by assuring migration will happen in right direction
+    and checking expected journey."""
+    mocker.patch('biosim.animals.uniform', return_value=0)
+    mocker.patch('biosim.world.choice', return_value='E')
+    island = World(geogr_str)
+    add_pop = [{'loc': (2, 2), 'pop': [{'species': 'Herbivore', 'age': 6, 'weight': 6.5}]}]
+    island.add_population(add_pop)
+    island.do_migration()
+
+    assert all([island.object_map[1, 2].population,
+                island.object_map[1, 2].herbivores,
+                len(island.object_map[1, 2].herbivores) == 1])
+
+
+@pytest.mark.parametrize('direction', ['N', 'S', 'W'])
+def test_migration_water(mocker, geogr_str, direction):
+    """Test that migration can not happen to water cells."""
+    mocker.patch('biosim.animals.uniform', return_value=0)
+    mocker.patch('biosim.world.choice', return_value=direction)
+    island = World(geogr_str)
+    add_pop = [{'loc': (2, 2), 'pop': [{'species': 'Herbivore', 'age': 6, 'weight': 6.5}]}]
+    island.add_population(add_pop)
+    island.do_migration()
+
+    assert not all([island.object_map[1, 2].population,
+                    island.object_map[0, 1].population,
+                    island.object_map[1, 0].population,
+                    island.object_map[2, 1].population,
+                    not island.object_map[1, 1].population])
 
 
 
