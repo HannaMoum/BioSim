9959cdb50a2ab6c09a7e453c3accce251a3855b2
Cassandra Hjortdahl <cassandra.hjortdahl@nmbu.no>
2022-01-16 19:54:19 +0100

    Remove comments.

--- src/biosim/run_simulaion.py
+++ src/biosim/run_simulaion.py
@@ -44,13 +44,13 @@ if __name__ == '__main__':
                  hist_specs={'fitness': {'max': 1.0, 'delta': 0.05},
                              'age': {'max': 60.0, 'delta': 2},
                              'weight': {'max': 60.0, 'delta': 2}},
-                 vis_years=None,
+                 vis_years=1,
                  ymax_animals=None,
                  cmax_animals={'Herbivore': 40, 'Carnivore': 10},
                  img_dir='C:/temp/BioSim/figs',
                  img_base='BioSim',
                  img_fmt='png',
-                 img_years=0
+                 img_years=1
                  )
 
     sim.set_animal_parameters('Herbivore', {'zeta': 3.2, 'xi': 1.8})
@@ -60,56 +60,6 @@ if __name__ == '__main__':
     sim.set_landscape_parameters('L', {'f_max': 800})
 
     sim.simulate(num_years=10)
-    sim.add_population(population=ini_carns)
-    #sim.simulate(num_years=10)
-
-    #sim.simulate(num_years=10)
-    #sim.make_movie()
-
-
-    # herb_pop_map = sim.get_property_map('v_size_herb_pop')
-
-    # plt.show()
-    # print(herbivore_property_array[:, 1])
-
-    # Plotter kartet over øya
-    # graf = Graphics(sim.island.base_map)
-
-    #graf.plot_island_map()
-    """
-    # Plotter begge populasjoner på samme ax
-    herb_count = sim.get_yearly_herb_count()
-    carn_count = sim.get_yearly_carn_count()
-    graf.plotting_population_count(herb_count, carn_count)
-    """
-    # kube1 = sim.cube_population_herbs
-    # kube2 = sim.cube_population_carns
-    #graf.plot_heatmap(kube1, species='herbivore')
-    #graf.plot_heatmap(kube2, species='carnivore')
-    # plt.show()
-
-    """herb_data = sim.cubelist_properties_herbs
-    carn_data = sim.cubelist_properties_carns
-
-    graf.plot_histogram(herb_data, carn_data)"""
-
-    # herb_count = sim.get_yearly_herb_count()
-    # carn_count = sim.get_yearly_carn_count()
-    # kube1 = sim.cube_population_herbs
-    # kube2 = sim.cube_population_carns
-    # herb_data = sim.cubelist_properties_herbs
-    # carn_data = sim.cubelist_properties_carns
-    #graf.show_panel(herb_count, carn_count, kube1, 'herbivore', herb_data, carn_data)
-
-    #graf.make_grid(10)
-
-    #sim.graphics.do_graphics(kube1, kube2, herb_count, carn_count, herb_data, carn_data)
-    #dash.make_movie()
-
-    # TODO: Bildene blir liggende i C:/temp/figs, og filmene blir liggende i C:/temp. Folderne må finnes på disk fra før, slik det er nå.
-    # dash.make_from_files(kube1, kube2, herb_count, carn_count, herb_data, carn_data, 29)
-    #dash.make_movie(kube1, kube2, herb_count, carn_count, herb_data, carn_data, 20)
-    # dash.make_grid(year=5)
-    # dash.make_movie()
-    #dash.make_from_files()
-    #plt.show()
+    # sim.add_population(population=ini_carns)
+    # sim.simulate(num_years=5)
+    # sim.simulate(num_years=3)
