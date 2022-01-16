9bc809543e6a313ba8b2b0176df9e2ab7c882656
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-16 19:11:33 +0100

    Add map_str as a fixture

--- tests/test_biosim_class.py
+++ tests/test_biosim_class.py
@@ -10,10 +10,13 @@ def hist_specs():
                   'weight': {'max': 60, 'delta': 2}}
     return hist_specs
 
+@pytest.fixture()
+def map_str():
+    return "WWW\nWLW\nWWW"
 
-def test_map_validation(hist_specs):
+
+def test_map_validation(hist_specs, map_str):
     """Test creation of World object if map input is valid."""
-    map_str = "WWW\nWLW\nWWW"
     sim = BioSim(map_str, hist_specs=hist_specs)
     assert type(sim.island) == World
 
@@ -22,4 +25,14 @@ def test_map_validation(hist_specs):
 def test_invalid_map(hist_specs, map):
     """Test that ValueErrors are risen when invalid map format is given."""
     with pytest.raises(ValueError):
-        sim = BioSim(map, hist_specs=hist_specs)
+        BioSim(map, hist_specs=hist_specs)
+
+
+
+
+
+def test_set_vis_years(hist_specs, map_str):
+    """Test that private setter function provides correct vis_years value."""
+    sim = BioSim(map_str, hist_specs=hist_specs, vis_years=5)
+    assert sim._vis_years == 5
+
