dbbaee92381be4c3c50f64a7f54fb86c6d4ec3ab
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-01-17 10:53:11 +0100

    Update check_sim to correct version

--- reference_examples/check_sim.py
+++ reference_examples/check_sim.py
@@ -50,14 +50,7 @@ if __name__ == '__main__':
                  hist_specs={'fitness': {'max': 1.0, 'delta': 0.05},
                              'age': {'max': 60.0, 'delta': 2},
                              'weight': {'max': 60, 'delta': 2}},
-                 vis_years=None,
-                 ymax_animals=None,
-                 cmax_animals={'Herbivore': 40, 'Carnivore': 10},
-                 img_dir='C:/temp/BioSim/figs',
-                 img_base='BioSim',
-                 img_fmt='png',
-                 img_years=0
-                 )
+                 vis_years=1)
 
     sim.set_animal_parameters('Herbivore', {'zeta': 3.2, 'xi': 1.8})
     sim.set_animal_parameters('Carnivore', {'a_half': 70, 'phi_age': 0.5,
