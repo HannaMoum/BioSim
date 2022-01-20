39c31c2b51f4252b0361865ee734e1f653037c38
Cassandra Hjortdahl <cassandra.hjortdahl@nmbu.no>
2022-01-20 10:47:59 +0100

    Adjust for movie making.

---
+++ src/biosim/movie.py
@@ -0,0 +1,59 @@
+# TODO: Move to examples, or delete before delivery (wait until tuesday).
+import textwrap
+from biosim.simulation import BioSim
+
+if __name__ == '__main__':
+    # plt.ion() Når du har skrudd av denne vil ikke figur-viduet dette sammen med en gang
+    geogr = """\
+                WWWWWWWWWWWWWWWWWWWWW
+                WWWWWWWWHWWWWLLLLLLLW
+                WHHHHHLLLLWWLLLLLLLWW
+                WHHHHHHHHHWWLLLLLLWWW
+                WHHHHHLLLLLLLLLLLLWWW
+                WHHHHHLLLDDLLLHLLLWWW
+                WHHLLLLLDDDLLLHHHHWWW
+                WWHHHHLLLDDLLLHWWWWWW
+                WHHHLLLLLDDLLLLLLLWWW
+                WHHHHLLLLDDLLLLWWWWWW
+                WWHHHHLLLLLLLLWWWWWWW
+                WWWHHHHLLLLLLLWWWWWWW
+                WWWWWWWWWWWWWWWWWWWWW"""
+    geogr = textwrap.dedent(geogr)
+
+    ini_herbs = [{'loc': (9, 9),
+                  'pop': [{'species': 'Herbivore',
+                           'age': 5,
+                           'weight': 20}
+                          for _ in range(150)]}]
+
+    ini_carns = [{'loc': (9, 9),
+                  'pop': [{'species': 'Carnivore',
+                           'age': 5,
+                           'weight': 20}
+                          for _ in range(40)]}]
+
+    sim = BioSim(island_map=geogr, ini_pop=ini_herbs+ini_carns,
+                 seed=123456,
+                 hist_specs={'fitness': {'max': 1.0, 'delta': 0.05},
+                             'age': {'max': 60.0, 'delta': 2},
+                             'weight': {'max': 60.0, 'delta': 2}},
+                 vis_years=None,
+                 img_fmt='png',
+                 img_years=1,
+                 img_dir='C:/temp/BioSim',
+                 img_base='BioSim'
+                 )
+
+    sim.simulate(num_years=400 )
+    sim.make_movie()
+    # sim.add_population(population=ini_carns)
+    # sim.simulate(num_years=5)
+    # sim.simulate(num_years=3)
+
+    # import cProfile
+    # import pstats
+    # from pstats import SortKey
+    #
+    # cProfile.run('sim.simulate(5)', 'restats')
+    # p = pstats.Stats('restats')
+    # p.sort_stats(SortKey.CUMULATIVE).print_stats('animals.py')
